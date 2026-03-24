78622a6641795a9a76079a0cc255ceea0b666d50
unknown <ctiguarulhos@ifsp.edu.br>
2026-03-24 12:47:47 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EMP</title>
+</head>
+<body>
+    <a href="cadastro/cadprof.php"><button>Cadastro</button></a>
+    
+    <button>Login</button>
+    
+</body>
+</html>
